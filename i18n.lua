d70534c8bc5d1d7b2397e217a83963c409d9ce21
Jesse van Herk <jesse@imaginaryrobots.net>
2016-07-10 22:24:33 -0600

    get i18n lib actually working

--- i18n.lua
+++ i18n.lua
@@ -1,7 +1,40 @@
+-- this is a namespace more than it is a module.
+
+local I18n = {}
+
+
+-- create new entity manager, pass in component data to use.
+function I18n.init( locale_strings )
+    I18n.strings = {}
+    I18n.locale_strings = locale_strings
+end
+
+function I18n.setLocale( locale )
+    I18n.locale = locale
+    if I18n.locale_strings[ locale ] then
+        I18n.strings = I18n.locale_strings[ locale ]
+    else
+        I18n.strings = {} -- go back to defaults.
+    end
+end
+
+-- main translation function. 
+-- this should be used ANY time you are OUTPUTTING a string. See dstr for data files though.
+function I18n.str( string )
+    local translation = string
+    if I18n.strings[ string ] then
+        translation = I18n.strings[ string ]
+    end
+
+    return translation
+end
+
 -- dummy translation function. 
 -- basically just makes it easy to find strings that need translating later.
-function i18n( string )
+-- this should mainly just be used in data files.
+function I18n.dstr( string )
     return string
 end
 
-return i18n
+
+return I18n
