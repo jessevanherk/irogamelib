f589873a371ee25fc5095f808e89add39bd75ed8
Jesse van Herk <jesse@imaginaryrobots.net>
2016-08-12 15:28:12 -0600

    move timers and signals into spaces

--- space.lua
+++ space.lua
@@ -16,23 +16,19 @@ function Space:new( ... )
 end
 
 -- create new space, pass in component data to use.
-function Space:_init( entity_templates, component_templates, create_cb, systems, system_prefix )
-    -- create an entity manager for this space
-    self.entity_manager = EntityManager:new( entity_templates, component_templates, create_cb )
+function Space:_init( entity_templates, component_templates, systems, system_prefix )
+    -- create signal handler and timer instances.
+    self.signal = Signal.new()
+    self.timer  = Timer.new()
 
-    -- create a timer instance, so timers only update when this space is active
-    --self.timer  = Timer.new()
-
-    -- create the systems that this space uses.
-    self.system_manager = SystemManager:new( systems )
-
-    -- FIXME: create signal handler
+    self.entity_manager = EntityManager:new( entity_templates, component_templates, self.onCreatedEntity )
+    self.system_manager = SystemManager:new( systems, system_prefix )
 end
 
 -- should be called once in the update callback.
 function Space:update( dt )
     -- update timers.
-    --self.timer:update( dt )
+    self.timer:update( dt )
     
     self.entity_manager:update( dt )
     self.system_manager:update( dt )
@@ -40,6 +36,32 @@ end
 
 function Space:draw()
     self.system_manager:draw()
+end
+
+-- register signal handlers. called from various systems
+-- return a set of handles that can be used to unregister them later
+function Space:registerSignalHandlers( signal_actions )
+    local signal_handles = {}
+    if signal_actions then
+        for signal, signal_action in pairs( signal_actions ) do
+            local handle = self.signal:register( signal, signal_action )
+            signal_handles[ signal ] = handle
+        end
+    end
+    return signal_handles
+end
+
+-- unregister signal handlers. called from various systems
+function Space:unregisterSignalHandlers( signal_handles )
+    for signal, handle in pairs( signal_handles ) do
+        self.signal:remove( signal, handle )
+    end
+end
+
+-- simple callback to tell systems/etc they need to do something.
+-- OVERRIDE this as needed.
+function Space:onCreatedEntity( entity )
+    self.signal:emit( "created_entity", entity )
 end
 
 return Space
