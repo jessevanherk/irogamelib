0f0575a5dddafd8be7a813dc41e8f078f5df8633
Jesse van Herk <jesse@imaginaryrobots.net>
2018-01-27 15:17:13 -0700

    fix indenting in gridgraph

--- graphs/gridgraph.lua
+++ graphs/gridgraph.lua
@@ -3,63 +3,63 @@ local GridGraph = {}
 local abs = math.abs
 
 function GridGraph:new( ... )
-    local instance = {}
-    setmetatable( instance, self )
-    self.__index = self
-    self._init( instance, ... )
-    return instance
+  local instance = {}
+  setmetatable( instance, self )
+  self.__index = self
+  self._init( instance, ... )
+  return instance
 end
 
 function GridGraph:_init( width, height, values, cost_cb )
-    self.width  = width
-    self.height = height
-    self.values = {}
+  self.width  = width
+  self.height = height
+  self.values = {}
 
-    self.cost_cb = self.default_cost_cb
-    if cost_cb then
-      self.cost_cb = cost_cb
-    end
+  self.cost_cb = self.default_cost_cb
+  if cost_cb then
+    self.cost_cb = cost_cb
+  end
 
-    for j = 1, height do
-        self.values[ j ] = {}
-        for i = 1, width do
-            if values and values[ j ] and values[ j ][ i ] then
-                self.values[ j ][ i ] = values[ j ][ i ]
-            end
-            -- else leave it as nil, so we can have a sparse array.
-        end
+  for j = 1, height do
+    self.values[ j ] = {}
+    for i = 1, width do
+      if values and values[ j ] and values[ j ][ i ] then
+        self.values[ j ][ i ] = values[ j ][ i ]
+      end
+      -- else leave it as nil, so we can have a sparse array.
     end
+  end
 end
 
 function GridGraph:get( point )
-    local value = nil
-    local x, y = point[ 1 ], point[ 2 ]
-    if x >= 1 and x <= self.width and y >= 1 and y <= self.height then
-        if self.values[ y ] then
-            value = self.values[ y ][ x ]
-        end
+  local value = nil
+  local x, y = point[ 1 ], point[ 2 ]
+  if x >= 1 and x <= self.width and y >= 1 and y <= self.height then
+    if self.values[ y ] then
+      value = self.values[ y ][ x ]
     end
-    return value
+  end
+  return value
 end
 
 function GridGraph:set( point, value )
-    local x, y = point[ 1 ], point[ 2 ]
-    if x >= 1 and x <= self.width and y >= 1 and y <= self.height then
-        if not self.values[ y ] then
-            self.values[ y ] = {}
-        end
-        self.values[ y ][ x ] = value
+  local x, y = point[ 1 ], point[ 2 ]
+  if x >= 1 and x <= self.width and y >= 1 and y <= self.height then
+    if not self.values[ y ] then
+      self.values[ y ] = {}
     end
+    self.values[ y ][ x ] = value
+  end
 end
 
 -- manhattan distance(?)
 function GridGraph:getHeuristic( node, goal )
-    local dx = abs( node[ 1 ] - goal[ 1 ] )
-    local dy = abs( node[ 2 ] - goal[ 2 ] )
-    local h = dx + dy
-    -- scale h by 1.001 to break ties, make nicer lines, and reduce search space.
-    h = h * 1.001
-    return h
+  local dx = abs( node[ 1 ] - goal[ 1 ] )
+  local dy = abs( node[ 2 ] - goal[ 2 ] )
+  local h = dx + dy
+  -- scale h by 1.001 to break ties, make nicer lines, and reduce search space.
+  h = h * 1.001
+  return h
 end
 
 -- get cost to move from current node to target node
@@ -74,41 +74,41 @@ end
 
 -- return a list of neighbour positions in no particular order
 function GridGraph:getNeighbours( point )
-    local neighbours = {}
+  local neighbours = {}
 
-    local x = point[ 1 ]
-    local y = point[ 2 ]
+  local x = point[ 1 ]
+  local y = point[ 2 ]
 
-    local north = { x, y - 1 }
-    local south = { x, y + 1 }
-    local east  = { x + 1, y }
-    local west  = { x - 1, y }
+  local north = { x, y - 1 }
+  local south = { x, y + 1 }
+  local east  = { x + 1, y }
+  local west  = { x - 1, y }
 
-    if x > 1 then
-        table.insert( neighbours, west )
-    end
-    if x < self.width then
-        table.insert( neighbours, east )
-    end
-    if y > 1 then
-        table.insert( neighbours, north )
-    end
-    if y < self.height then
-        table.insert( neighbours, south )
-    end
+  if x > 1 then
+    table.insert( neighbours, west )
+  end
+  if x < self.width then
+    table.insert( neighbours, east )
+  end
+  if y > 1 then
+    table.insert( neighbours, north )
+  end
+  if y < self.height then
+    table.insert( neighbours, south )
+  end
 
-    return neighbours
+  return neighbours
 end
 
 function GridGraph:getNeighbourValues( point )
-    local neighbours = self:getNeighbours( point )
+  local neighbours = self:getNeighbours( point )
 
-    local values = {}
-    for _, neighbour in pairs( neighbours ) do
-        table.insert( values, self:get( neighbour ) )
-    end
+  local values = {}
+  for _, neighbour in pairs( neighbours ) do
+    table.insert( values, self:get( neighbour ) )
+  end
 
-    return values
+  return values
 end
 
 -- by default, movement costs are all 1.
